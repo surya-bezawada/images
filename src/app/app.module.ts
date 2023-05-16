4dd1518468a9ad197fdcf3d797f21cfd0425fb8f
Surya@95 <surya.t@imperiumsoft.biz>
2023-05-16 19:55:32 +0530

    footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './core/Layout/header.component';
+import { HeaderComponent } from './core/Layout/header/header.component';
+import { FooterComponent } from './core/Layout/Footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   
   ],
   imports: [
